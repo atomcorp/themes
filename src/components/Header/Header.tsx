c0859e2717cba0b714a0af941020dedbbb501a19
atomcorp <thomasmaxwellsmith@gmail.com>
2019-10-29 09:46:22 +0000

    change name

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,14 +4,13 @@ import css from './Header.module.css';
 
 const Header: React.FC = () => (
   <section className={css.container}>
-    <h1 className={css.title}>Windows Terminal Colours</h1>
-    Themes from{' '}
+    <h1 className={css.title}>Windows Terminal Themes</h1>
     <a
       target="_blank"
       rel="noopener noreferrer"
       href="https://github.com/mbadolato/iTerm2-Color-Schemes"
     >
-      iTerm2 Color Schemes
+      via iTerm2 Color Schemes
     </a>
     {' | '}
     <a target="_blank" href="/colour-schemes.json">
