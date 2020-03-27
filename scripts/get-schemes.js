48546f402835b217b18fdcc3f289e7bdc3cd3419
tom <thomasmaxwellsmith@gmail.com>
2020-03-27 17:12:18 +0000

    better get schemes, proper auth, more readable

--- scripts/get-schemes.js
+++ scripts/get-schemes.js
@@ -1,46 +1,48 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const request = require('request');
+const got = require('got');
 const fs = require('fs');
-const path = require('path');
-const requestPromise = require('request-promise-native');
 const customSchemaJson = require('../src/custom-colour-schemes.json');
+const private = require('../private.json');
 
+const btoa = (str) => Buffer.from(str, 'binary').toString('base64');
 const options = {
-  json: true,
   headers: {
-    'User-Agent': 'atomcorp',
+    'User-Agent': 'Windows Terminal Themes',
+    Authorization: `Basic ${btoa(`atomcorp:${private.token}`)}`,
+    Accept: 'application/vnd.github.v3.raw',
   },
 };
+const baseUrl =
+  'https://api.github.com/repos/mbadolato/iTerm2-Color-Schemes/contents/windowsterminal/';
 
-// get all the file names from the directory
-const FILES_PATH =
-  'https://api.github.com/repos/mbadolato/iTerm2-Color-Schemes/contents/windowsterminal';
-// this is just an easier way of downloading the raw data than the API stuff
-const RAW_PATH =
-  'https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/windowsterminal/';
-
-const iTerm2SchemaJson = [];
-
-request(FILES_PATH, options, (err, response, body) => {
-  Promise.all(
-    body.map((file) =>
-      requestPromise(
-        `${RAW_PATH}${file.name}`,
-        options,
-        (err, response, body) => {
-          iTerm2SchemaJson.push(body);
-        }
-      )
-    )
-  ).then(() => {
-    const combinedSchemaJson = [...iTerm2SchemaJson, ...customSchemaJson].sort(
-      (a, b) => {
-        return a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1;
-      }
+const main = async () => {
+  try {
+    // get the list of scheme names in the git repo directory
+    const dirResponse = await got(baseUrl, options);
+    const files = JSON.parse(dirResponse.body);
+    // use those names to download each of the scheme pages
+    const fileResponses = await Promise.all(
+      files.map((file) => got({...options, url: `${baseUrl}${file.name}`}))
     );
+    // turn into the JSON
+    const iTerm2SchemaJson = fileResponses.map((fileResponse) =>
+      JSON.parse(fileResponse.body)
+    );
+    // merge with any themes that have been added in this project
+    const combinedSchemaJson = [
+      ...iTerm2SchemaJson,
+      ...customSchemaJson,
+    ].sort((a, b) => (a.name.toUpperCase() > b.name.toUpperCase() ? 1 : -1));
+    // write the new file
     fs.writeFileSync(
-      path.join('src', 'colour-schemes.json'),
+      './src/colour-schemes.json',
       JSON.stringify(combinedSchemaJson, null, 2)
     );
-  });
-});
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+  }
+};
+
+main();
