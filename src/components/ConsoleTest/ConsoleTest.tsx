064735b82bb30ddd25ea8f38459518c83eee8382
tom <thomasmaxwellsmith@gmail.com>
2020-05-06 17:31:46 +0100

    parse and displaymarkup with colour names

--- src/components/ConsoleTest/ConsoleTest.tsx
+++ src/components/ConsoleTest/ConsoleTest.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import css from './ConsoleTest.module.css';
 import {themeType, validKeysType, textKeyType} from 'types';
+import {validKeys} from 'components/ThemePreview/consoleMethods';
+
 type PropsType = {
   theme: themeType;
 };
@@ -9,17 +11,46 @@ type codeblocksType = {
   [key: string]: {
     name: string;
     markup: string;
-    defaultColour: textKeyType;
+    defaultColour: textKeyType | 'foreground' | 'background';
   };
 };
 
 const setParseSyntax = (theme: themeType) => (markup: string) => {
-  return markup.split(/(<.+?>.+<\/.+>)/g).map((string) => {
+  console.log(markup.split(/(<[^/>]+?>[^<]+<[^>]+?>)/g));
+  return markup.split(/(<[^/>]+?>[^<]+<[^>]+?>)/g).map((string, i) => {
     const matches = [...string.matchAll(/<(.+?)>(.+)<\/(.+)>/g)][0];
     if (matches != null) {
-      const colour = matches[1] as textKeyType;
+      if (process.env.NODE_ENV === 'development' && matches[1] !== matches[3]) {
+        throw new Error(
+          `Opening tag <${matches[1]}> does not match closing tag </${matches[3]}>`
+        );
+      }
+      const colours = matches[1].split(':');
+      const foreground = colours[0] as textKeyType;
+      const background =
+        colours.length > 0 ? (colours[1] as textKeyType) : null;
+      console.log(matches);
+      if (
+        process.env.NODE_ENV === 'development' &&
+        (!validKeys.includes(foreground) ||
+          (background && !validKeys.includes(background)))
+      ) {
+        throw new Error(
+          `Using invalid tags: <${matches[1]}>${matches[2]}</${matches[3]}>`
+        );
+      }
       const contents = matches[2];
-      return <span style={{color: theme[colour]}}>{contents}</span>;
+      return (
+        <span
+          key={i}
+          style={{
+            color: theme[foreground],
+            background: background ? theme[background] : undefined,
+          }}
+        >
+          {contents}
+        </span>
+      );
     }
     return string;
   }, []);
@@ -36,9 +67,75 @@ const codeblocks: codeblocksType = {
     http://192.168.86.37:3000/themes Note that the development build is not
     optimized. 
 
-    To create a production build, use{' '}
+    To create a production build, use
     <cyan>yarn build</cyan>.`,
     defaultColour: 'white',
+  },
+  cypressTest: {
+    name: 'Cypress',
+    markup: `
+    <foreground>Running:</foreground>  <brightBlack>themes.spec.js                           (1 of 1)</brightBlack>
+
+
+    <foreground>Windows Terminal Themes - big screen</foreground>
+    <cyan>- should download all themes using download button</cyan>
+    <green>√</green> should show dark theme by default <red>(705ms)</red>
+    
+    <green>(</green><brightGreen>Results</brightGreen><green>)</green>
+
+    ┌──────────────────────────────────────────────────────────┐
+    │ Tests:        <green>18</green>                                         │
+    │ Duration:     <green>10 seconds</green>                                 │
+    │ Spec Ran:     <green>themes.spec.js</green>                             │
+    └──────────────────────────────────────────────────────────┘
+    `,
+    defaultColour: 'brightBlack',
+  },
+  jestFailed: {
+    name: 'Jest: failed test',
+    markup: `
+    <foreground:brightRed> FAIL </foreground:brightRed> src/components/Home/Home.test.js (<white:red>17.523s</white:red>)
+      <red>● should return theme name from search params</red>
+  
+      expect(<red>received</red>).toBe(<green>expected</green>) // Object.is equality
+  
+        Expected: <green>"synthwave-everything</green><background:brightGreen>error"</background:brightGreen>
+        Received: <red>"synthwave-everything"</red>
+    
+          218 |
+          219 | it(<green>'should return theme name from search params'</green>, () <yellow>=></yellow> {
+        > 220 |   expect(returnInitialTheme(<brightGreen>'?theme=synthwave-everything'</brightGreen>)).toBe(
+              |                                                             ^
+          221 |     <green>'synthwave-everythingerror'</green>
+          222 |   );
+          223 |   expect(returnInitialTheme(<green>'?wrong=synthwave-everything'</green>)).toBe(<cyan>null</cyan>);
+        
+          at Object.toBe (<cyan>src/components/Home/Home.test.js</cyan>:220:61)
+  
+    Test Suites: <brightRed>1 failed</brightRed>, <brightGreen>1 passed</brightGreen>, 2 total
+    Tests:       <brightRed>1 failed</brightRed>, <brightYellow>4 skipped</brightYellow>, <brightGreen>2 passed</brightGreen>, 7 total
+    Snapshots:   0 total
+    Time:        <brightYellow>22.5s</brightYellow>
+    Ran all test suites.
+
+    Watch Usage: Press w to show more.
+    `,
+    defaultColour: 'white',
+  },
+  jestRunning: {
+    name: 'Jest - running',
+    markup: `
+    <white:brightYellow>  RUNS  </white:brightYellow> src/components/Home/Home.test.js
+    <white:brightYellow>  RUNS  </white:brightYellow>  src/App.test.tsx
+    
+    Test Suites: 0 of 2 total
+    Tests:       0 total
+    Snapshots:   0 total
+    Time:        4s
+    <background:green>         </background:green><background:foreground>                             </background:foreground>
+
+    `,
+    defaultColour: 'foreground',
   },
 };
 
@@ -50,9 +147,9 @@ const ConsoleTest: React.FC<PropsType> = (props) => {
       <div className={css.titlebar}>Title bar, tabs, close</div>
       <code
         className={css.code}
-        style={{color: props.theme[codeblocks.craCompiled.defaultColour]}}
+        style={{color: props.theme[codeblocks.cypressTest.defaultColour]}}
       >
-        {parseSyntax(codeblocks.craCompiled.markup)}
+        {parseSyntax(codeblocks.cypressTest.markup)}
       </code>
     </section>
   );
