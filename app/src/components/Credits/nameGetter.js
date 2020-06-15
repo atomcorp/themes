f8a430dec07a21f2d8fe7ea845f16cbeb0578614
Savvasun <savvazhukov2008@gmail.com>
2020-06-15 11:32:33 -0700

    Copy JS File

---
+++ app/src/components/Credits/nameGetter.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const themes = require('Credits/namesAndThemes.json');
+const themenames = themes.map((theme) => theme.name);
+const buffer = fs.readFileSync('Credits\README.md');
+const markdownCredits = buffer.toString();
+
+const credits = themenames.map((themename) => {
+  const note = markdownCredits.match(new RegExp(`(.+)${themename}(.+)`, 'i'));
+  return {
+    theme: themename,
+    notes: note != null ? note[0] : 'notfound',
+  };
+});
+
+fs.writeFileSync('Credits\namesAndThemes.json', JSON.stringify(credits, null, 2));
