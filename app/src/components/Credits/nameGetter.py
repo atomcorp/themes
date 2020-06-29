067436938d3d230ff470f9ce47b43f527d9c2530
Savvasun <savvazhukov2008@gmail.com>
2020-06-29 12:44:10 -0700

    Update Algorithm

--- app/src/components/Credits/nameGetter.py
+++ app/src/components/Credits/nameGetter.py
@@ -9,23 +9,42 @@ infoFile = open(r'app\src\components\Credits\namesAndThemes.json', 'wt')
 
 #Declares variables and files for parsing
 namesAndCreators = {}
-themeNamesBuffer = open(r'server\themes.json')
-OGThemes = json.load(themeNamesBuffer)
-themes = list(map(lambda x : x['name'], OGThemes))
+themesBuffer = open(r'server\themes.json')
+themes = json.load(themesBuffer)
 
 #Finds a match for every theme name in the credits file
 #'r' is the current theme name
 def credit(r):
+    returnObj = {}
+
     for i in range(len(namesList)):
         line = namesList[i]
         lineWords = line.split()
 
         for e in range(len(lineWords)):
-          if (r.split()[0] == lineWords[e])
-            return {
-                'name': r,
-                'note': line
+            try:
+                if (r['name'].split()[0] == lineWords[e] and r['name'].split()[1] == lineWords[e + 1]):
+                    returnObj =  {
+                        'name': r['name'],
+                        'note': line,
+                        'dark': r['isDark']
+                    }
+            except:
+                if (r['name'].split()[0] == lineWords[e]):
+                    returnObj =  {
+                        'name': r['name'],
+                        'note': line,
+                        'dark': r['isDark']
+                    }
+
+        if (returnObj == {}):
+            returnObj = {
+                'name': r['name'],
+                'note': 'Not Found',
+                'dark': r['isDark']
             }
+
+    return returnObj
 
 namesAndCreators['credits'] = list(map(credit, themes))
 
