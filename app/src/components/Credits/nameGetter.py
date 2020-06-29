ba7f8b3b3b95f157a3f536a48675db6e2667a54b
Savvasun <savvazhukov2008@gmail.com>
2020-06-29 17:21:39 +0000

    Further Shorten Algorithm

--- app/src/components/Credits/nameGetter.py
+++ app/src/components/Credits/nameGetter.py
@@ -2,15 +2,10 @@ import json
 
 #Opens the credits markdown file
 names = open(r'app\src\components\Credits\Credits.md')
-namesText = names.read()
-namesList = namesText.split('\n')
+namesList = names.read().split('\n')
 
-#Opens the file and resets to a blank line
+#Opens the output file for writing
 infoFile = open(r'app\src\components\Credits\namesAndThemes.json', 'wt')
-infoFile.write('')
-infoFile.close()
-#Reopens the file to write the output
-infoFile = open(r'app\src\components\Credits\namesAndThemes.json', 'at')
 
 #Declares variables and files for parsing
 namesAndCreators = {}
@@ -18,6 +13,8 @@ themeNamesBuffer = open(r'server\themes.json')
 OGThemes = json.load(themeNamesBuffer)
 themes = list(map(lambda x : x['name'], OGThemes))
 
+#Finds a match for every theme name in the credits file
+#'r' is the current theme name
 def credit(r):
     for i in range(len(namesList)):
         line = namesList[i]
@@ -26,11 +23,11 @@ def credit(r):
         for e in range(len(lineWords)):
           if (r.split()[0] == lineWords[e])
             return {
-                "name": r,
-                "note": line
+                'name': r,
+                'note': line
             }
 
-namesAndCreators['Credits'] = list(map(credit, themes))
+namesAndCreators['credits'] = list(map(credit, themes))
 
 json = json.dumps(namesAndCreators, indent = 4, sort_keys = True)
 infoFile.write(json)
