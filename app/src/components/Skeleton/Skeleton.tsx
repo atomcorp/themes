12b0ac7eaa600a6cc3ff743630e59e9dfe5e8ccd
atomcorp <thomasmaxwellsmith@gmail.com>
2020-07-14 23:36:18 +0100

    slightly less bad loading text

--- app/src/components/Skeleton/Skeleton.tsx
+++ app/src/components/Skeleton/Skeleton.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import {Logo} from 'Icons';
+
 import css from './Skeleton.module.css';
 
 const Skeleton = (): JSX.Element => (
   <section className={css.container}>
-    <h1>Windows Terminal Themes</h1>
+    <h1>
+      <Logo
+        size="48px"
+        colours={['#8adbb4', '#d7af87', '#c79ec4']}
+        className={css.logo}
+      />
+      Windows Terminal Themes
+    </h1>
     <h4>Loading...</h4>
   </section>
 );
