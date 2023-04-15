676ad98f6675673141608a44c97761373f9ebb04
atomcorp <thomasmaxwellsmith@gmail.com>
2023-04-15 22:25:07 +0100

    rm legacy files

--- legacy/old-react/src/declarations.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'get-contrast';
-declare module 'immer';
-declare module 'resize-observer-polyfill';
-declare module '*.module.css';
--- legacy/server/run-themes.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const getThemes = require('./get-themes');
-
-getThemes();
