06a986d1722934c67cd326203eba7215f19945fd
atomcorp <thomasmaxwellsmith@gmail.com>
2023-07-21 16:54:06 +0100

    remove page tests

--- app/list/layout.tsx
+++
@@ -1,9 +0,0 @@
-import useColorSchemes from '@/components/ColorSchemeContext/useColorSchemes';
-
-export default function Layout(props: {
-  children: React.ReactNode;
-  light: React.ReactNode;
-  dark: React.ReactNode;
-}) {
-  return <>{props.children}</>;
-}
